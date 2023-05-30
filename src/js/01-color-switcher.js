ca36a88bf30b9268b294ecf492658f302a2d8b34
Igor <125488812+Igor-Chumak@users.noreply.github.com>
2023-05-30 22:46:16 +0300

    task1 ok

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const ref = {
+  btnStart: document.querySelector('[data-start]'),
+  btnStop: document.querySelector('[data-stop]'),
+};
 
+ref.btnStop.setAttribute('disabled', '');
+
+ref.btnStart.addEventListener('click', () => {
+  ref.btnStart.setAttribute('disabled', '');
+  ref.btnStop.removeAttribute('disabled');
+  timerId = setInterval(() => {
+    document.body.style.background = getRandomHexColor();
+  }, 1000);
+});
+
+ref.btnStop.addEventListener('click', () => {
+  clearInterval(timerId);
+  ref.btnStart.removeAttribute('disabled');
+  ref.btnStop.setAttribute('disabled', '');
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
