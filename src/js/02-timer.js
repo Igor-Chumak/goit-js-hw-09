d1508e4550ad3f6a76bfae6519f0bf491a3afa06
Igor <125488812+Igor-Chumak@users.noreply.github.com>
2023-05-31 00:54:05 +0300

    task2 1

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -3,30 +3,35 @@ import 'flatpickr/dist/flatpickr.min.css';
 
 const ref = {
   btnStart: document.querySelector('[data-start]'),
-  //   input: document.getElementById('datetime-picker'),
-  dataDays: document.querySelector('[data-start]'),
+  input: document.getElementById('datetime-picker'),
+  dataDays: document.querySelector('[data-days]'),
   dataHours: document.querySelector('[data-hours]'),
   dataMinutes: document.querySelector('[data-minutes]'),
   dataSeconds: document.querySelector('[data-seconds]'),
 };
 ref.btnStart.setAttribute('disabled', '');
 
-const dateInput = flatpickr('#datetime-picker', {
+const dateInput = flatpickr(ref.input, {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    currentDate = new Date();
-    if (currentDate.getTime() > selectedDates[0].getTime()) {
+    let timeStamp = selectedDates[0].getTime() - new Date().getTime();
+    // console.log('cur: ', new Date().getTime());
+    // console.log('sel: ', selectedDates[0].getTime());
+    // console.log('d: ', timeStamp);
+    if (timeStamp <= 0) {
       window.alert('Please choose a date in the future');
       return selectedDates[0];
     }
-    console.log(selectedDates[0]);
-    ref.dataDays.textContent(selectedDates[0].getDate());
-    ref.dataHours.textContent(selectedDates[0].getHours());
-    ref.dataMinutes.textContent(selectedDates[0].getMinutes());
-    ref.dataSeconds.textContent(selectedDates[0].getSeconds());
+    const { days, hours, minutes, seconds } = convertMs(timeStamp);
+    ref.dataDays.textContent = addLeadingZero(days);
+    ref.dataHours.textContent = addLeadingZero(hours);
+    ref.dataMinutes.textContent = addLeadingZero(minutes);
+    ref.dataSeconds.textContent = addLeadingZero(seconds);
+
+    ref.btnStart.removeAttribute('disabled');
   },
 });
 
@@ -47,4 +52,8 @@ function convertMs(ms) {
   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
   return { days, hours, minutes, seconds };
+}
+
+function addLeadingZero(value) {
+  return value > 9 ? String(value) : String(value).padStart(2, '0');
 }
