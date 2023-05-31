ad8f36dc50fed1f53ed9e383230716e09aaea6aa
Igor <125488812+Igor-Chumak@users.noreply.github.com>
2023-06-01 00:54:38 +0300

    3 start

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,18 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const ref = {
+  form: document.querySelector('.form'),
+};
+
+ref.form.addEventListener('submit', onSubmit);
+
+function onSubmit(e) {
+  e.preventDefault();
+  const delayValue = refs.form.elements.delay.value;
+
+  // check on 0 ?
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
