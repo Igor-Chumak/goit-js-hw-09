c883bbd3b8ae8b7592e447a88550eaa38d7c7705
Igor <125488812+Igor-Chumak@users.noreply.github.com>
2023-06-03 14:50:52 +0300

    3 ok

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -12,43 +12,48 @@ const refs = {
   btnSubmit: document.querySelector('[type="submit"]'),
 };
 
-// temporary
-refs.form.elements.delay.value = '1000';
-refs.form.elements.step.value = '1000';
-refs.form.elements.amount.value = '2';
+// initial value
+// refs.form.elements.delay.value = '1000';
+// refs.form.elements.step.value = '1000';
+// refs.form.elements.amount.value = '3';
 
 refs.form.addEventListener('submit', onSubmit);
 
 function onSubmit(e) {
   e.preventDefault();
-  const delayValue = refs.form.elements.delay.value;
-  const stepValue = refs.form.elements.step.value;
-  const amountValue = refs.form.elements.amount.value;
+  const delayValue = +refs.form.elements.delay.value;
+  const stepValue = +refs.form.elements.step.value;
+  const amountValue = +refs.form.elements.amount.value;
+
   if (checkCorrectValue({ delayValue, stepValue, amountValue })) {
     return;
   }
-  console.log(
-    `delay: ${delayValue} step: ${stepValue}  amount: ${amountValue}`
-  );
-  refs.btnSubmit.disabled = true;
 
+  refs.btnSubmit.disabled = true;
+  let position = 1;
   let delay = delayValue;
-  createPromise((position = 1), delay)
-    .then(({ position, delay }) => {
-      Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
-      // console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-    })
-    .catch(({ position, delay }) => {
-      Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
-      // console.error(`❌ Rejected promise ${position} in ${delay}ms`);
-    })
-    .finally(() => (refs.btnSubmit.disabled = false));
+
+  for (let i = 0; i < amountValue; i += 1) {
+    position = i + 1;
+    delay = delayValue + stepValue * i;
+
+    createPromise(position, delay)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+        // console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+        // console.error(`❌ Rejected promise ${position} in ${delay}ms`);
+      })
+      .finally(() => (refs.btnSubmit.disabled = false));
+  }
 }
 
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   return new Promise((resolve, rejection) => {
-    console.log(`Promise ${position} at delay ${delay} created`);
+    // console.log(`Promise ${position} at delay ${delay} created`);
     setTimeout(() => {
       if (shouldResolve) {
         // Fulfill
