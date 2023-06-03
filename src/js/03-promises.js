537915b1fbfa5b6fb235896ccc60af57ea440506
Igor <125488812+Igor-Chumak@users.noreply.github.com>
2023-06-03 13:18:48 +0300

    3 key

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,7 +1,15 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
+Notify.init({
+  width: '500px',
+  fontSize: '25px',
+  position: 'center-top',
+  opacity: 0.7,
+  timeout: 1500,
+});
 
 const refs = {
   form: document.querySelector('.form'),
+  btnSubmit: document.querySelector('[type="submit"]'),
 };
 
 // temporary
@@ -16,17 +24,13 @@ function onSubmit(e) {
   const delayValue = refs.form.elements.delay.value;
   const stepValue = refs.form.elements.step.value;
   const amountValue = refs.form.elements.amount.value;
-  if (delayValue < 0 || stepValue < 0 || amountValue < 1) {
-    console.error(
-      ` ${delayValue < 0 ? 'Delay cannot be more than 0  ' : ''}${
-        stepValue < 0 ? 'Step cannot be less than 0  ' : ''
-      }${amountValue < 1 ? 'Amount must be more than 0' : ''}`
-    );
+  if (checkCorrectValue({ delayValue, stepValue, amountValue })) {
     return;
   }
   console.log(
     `delay: ${delayValue} step: ${stepValue}  amount: ${amountValue}`
   );
+  refs.btnSubmit.disabled = true;
 }
 
 // createPromise(2, 1500)
@@ -41,7 +45,31 @@ function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
     // Fulfill
+    Resolve(position, delay);
   } else {
     // Reject
+    Rejection(position, delay);
   }
+}
+
+function checkCorrectValue({ delayValue, stepValue, amountValue }) {
+  let errorAnyValue = false;
+
+  if (delayValue < 0) {
+    Notify.warning('Delay cannot be less than 0');
+    errorAnyValue = true;
+  }
+
+  if (stepValue < 0) {
+    Notify.warning('Step cannot be less than 0');
+
+    errorAnyValue = true;
+  }
+
+  if (amountValue < 1) {
+    Notify.warning('Amount must be more than 0');
+    errorAnyValue = true;
+  }
+
+  return errorAnyValue;
 }
